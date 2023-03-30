4be575bd43c6168d1bcbc1530b864cf24281e4d3
Arun <arun.n61@gmail.com>
2023-03-30 21:42:52 +0530

    updated

--- src/redux/sagas/BookingPlanSaga.js
+++ src/redux/sagas/BookingPlanSaga.js
@@ -7,9 +7,12 @@ import { apiCall, mockApi } from "../../utils/Api";
 function* fetchPlansFn(action) {
     try {
       let fetchPlanUrl = pageURL.FETCH_PLANS;
-      let response = yield call(apiCall, "post", fetchPlanUrl, action.data);
+      //let response = yield call(apiCall, "post", fetchPlanUrl, action.data);
+      let response;
       if (mockApi) {
         response = fetchPlanMock;
+      }else {
+        response = yield call(apiCall, "post", fetchPlanUrl, action.data);
       }
       if (response?.data?.success === 1) {
         yield put({ type: FETCH_PLANS_SUCCESS, data: response.data });
